8ff04f5ec3b9f6ae049b9ef3928d1b07232a65a6
David Swanson <davidswanson@Davids-MacBook-Pro-2.local>
2014-06-17 13:25:13 -0400

    first commit

---
+++ R/rpackage_test-package.r
@@ -0,0 +1,5 @@
+#' rpackage_test.
+#'
+#' @name rpackage_test
+#' @docType package
+NULL
---
+++ R/test.R
@@ -0,0 +1,18 @@
+
+#' test function
+#'
+#' This function allows you to sum 2 numbers without the addition sign
+#' @param
+#' param1 first parameter of addition operation
+#' param2 first parameter of addition operation
+#' @keywords addition summing
+#' @export
+#' @examples
+#' test(5,6)
+#' test(-5,8)
+
+test <- function(param1,param2){
+	
+	param1 + param2
+	
+}
